3c4563415d71cfda377a9b8d3ec728ff5addb61c
Gjergj Kadriu <gjergjk71@gmail.com>
2022-04-05 22:40:04 +0200

    editor-js/part-1

--- components/SinglePost/index.js
+++ components/SinglePost/index.js
@@ -1,22 +1,77 @@
 import Link from "next/link";
 import styles from "./SinglePost.module.css";
+// import EditorJS from '@editorjs/editorjs';
+import { useEffect, useRef, useState } from "react";
+  
 
+const edjsHTML = require("editorjs-html");
+const edjsParser = edjsHTML();
+
+console.log({ edjsParser })
 export default function SinglePost({ post, html_mode }) {
+    if (post.id < 5) return "OK"
+    const [ editor, setEditor ] = useState(null);
+    const holder = useRef(null);
+    console.log({ post })
+    // const html = edjsParser.parseStrict(post.html_content);
+
+    useEffect(() => {
+        if (typeof window === "undefined") return;
+        const Embed =  require('@editorjs/embed');
+        const Table =  require('@editorjs/table');
+        const List =  require('@editorjs/list');
+        const Warning =  require('@editorjs/warning');
+        const Code =  require('@editorjs/code');
+        const LinkTool =  require('@editorjs/link');
+        const Image =  require('@editorjs/image');
+        const Raw =  require('@editorjs/raw');
+        const Header =  require('@editorjs/header');
+        const Quote =  require('@editorjs/quote');
+        const Marker =  require('@editorjs/marker');
+        const CheckList =  require('@editorjs/checklist');
+        const Delimiter =  require('@editorjs/delimiter');
+        const InlineCode =  require('@editorjs/inline-code');
+        const SimpleImage =  require('@editorjs/simple-image');
+        
+        const EDITOR_JS_TOOLS = {
+            embed: Embed,
+            table: Table,
+            marker: Marker,
+            list: List,
+            warning: Warning,
+            code: Code,
+            linkTool: LinkTool,
+            image: Image,
+            raw: Raw,
+            header: Header,
+            quote: Quote,
+            checklist: CheckList,
+            delimiter: Delimiter,
+            inlineCode: InlineCode,
+            simpleImage: SimpleImage,
+        };
+        const { createReactEditorJS } = require("react-editor-js")
+        const ReactEditorJS = createReactEditorJS({ readOnly: true });
+
+        console.log({ post });
+        const editor = <ReactEditorJS
+            defaultValue={JSON.parse(post.html_content)}
+            tools={EDITOR_JS_TOOLS}
+            readOnly={true}
+        />
+        setEditor(editor)
+    }, [ holder ])
+    
+    if (!editor) "editor loading"
     return (
         <div class="">
             <Link href={`/posts/${post.slug}`}>
                 <h4 style={{ cursor: 'pointer' }}>{post.title}</h4>
             </Link>
             {
-                !html_mode 
+                !html_mode
                     ? <p>{post.meta.content_text}</p>
-                    : (
-                        <div style={{
-                            lineHeight: 1
-                        }} dangerouslySetInnerHTML={{
-                            __html: post.html_content
-                        }}></div>
-                    )
+                    : (editor)
             }
             <div style={{ display: "grid" }}>
                 <div style={{ display: 'inline-flex'}}>
